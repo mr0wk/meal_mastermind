4d325a8c3ff0d6ab1dba0acfb8fcfbe87336e054
Maksymilian <s25940@pjwstk.edu.pl>
2024-05-06 14:09:38 +0200

    first commit

---
+++ meals/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Meal(models.Model):
+    class MealType(models.TextChoices):
+        BREAKFAST = "breakfast"
+        LUNCH = "lunch"
+        DINNER = "dinner"
+        SNACK = "snack"
+
+    name = models.CharField(max_length=255)
+    meal_type = models.CharField(
+        max_length=10,
+        choices=[(tag.value, tag.value) for tag in MealType],
+        default=MealType.BREAKFAST.value,
+    )
+    date = models.DateTimeField()
+    products = models.ManyToManyField("products.Product", related_name="meals")
+
+    def __str__(self):
+        return self.name
